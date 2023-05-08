cdf78f7054c3e6888a0869404c858dc2ccad0683
FrozenBuffalo1406 <kenapagaadalagi@gmail.com>
2023-05-08 18:36:33 +0700

    editing show.java

--- UTS_kuliner/src/Show.java
+++ UTS_kuliner/src/Show.java
@@ -2,7 +2,8 @@ public class Show {
     public Show(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
-        System.out.println("Jumlah Pengunjung Diharapkan: " + Appetizer.harga / Checkout.totalKeuntungan);
+        
+        System.out.println("Jumlah Pengunjung Diharapkan: " + Checkout.totalOmset / Checkout.totalKeuntungan);
         System.out.println("Jumlah Pengunjung: " + Main.jumlahPengunjung);
         System.out.println("Total Keuntungan: " + Checkout.totalKeuntungan);
         Main.Menu();
